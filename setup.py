59b03a0b85a38dfea60a32f56e8f6fb38ebe6219
DevilsAutumn <bhuvnesh875@gmail.com>
2025-07-07 00:27:21 +0530

    fallback version changed

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def get_version():
         for line in f:
             if line.startswith("__version__"):
                 return line.split("=")[1].strip().strip("\"'")
-    return "0.1.0"
+    return "0.1.0-alpha"
 
 
 setup(
